60b4a9a65904eef65ea7df129d312f4dcc218a81
JihoonRyu00 <44030644+JihoonRyu00@users.noreply.github.com>
2024-06-19 03:53:08 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-# 프로젝트 디렉터리로 이동
-cd /home/ubuntu/environment/new_deploy
+# 프로젝트 디렉토리로 이동
+cd /home/ubuntu/environment/
 
 # 서버 시작
-source venv/bin/activate
 streamlit run chatbot_streamlit.py &
 
 cd InT
 
-python manage.py runserver 0.0.0.0:8000 &
+python3 manage.py runserver 0.0.0.0:8000 &

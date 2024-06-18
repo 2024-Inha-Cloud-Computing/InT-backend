a17f3576c90060919cd68926c52d805f86a289fa
JihoonRyu00 <44030644+JihoonRyu00@users.noreply.github.com>
2024-06-19 05:27:03 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,8 +4,14 @@
 cd /home/ubuntu/environment/new_deploy
 
 # 서버 시작
-streamlit run chatbot_streamlit.py &
+#streamlit run chatbot_streamlit.py &
+
+pwd > pwd1.txt
+ls > ls1.txt 
 
 cd InT
+
+pwd > pwd.txt
+ls > ls.txt 
 
 python manage.py runserver 0.0.0.0:8000 &

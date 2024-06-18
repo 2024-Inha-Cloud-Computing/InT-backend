f8934578f5e13256f7c6fac1cab96246b6e7fa1f
JihoonRyu00 <44030644+JihoonRyu00@users.noreply.github.com>
2024-06-19 05:04:37 +0900

    Update initialize.sh

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -3,10 +3,6 @@
 # 프로젝트 디렉토리로 이동
 cd /home/ubuntu/environment/new_deploy
 
-# Python 가상 환경 설정 및 활성화
-python3 -m venv venv
-source venv/bin/activate
-
 # 운영체제 패키지 업데이트
 sudo apt-get update
 sudo apt-get install -y build-essential libssl-dev libffi-dev python3-dev
